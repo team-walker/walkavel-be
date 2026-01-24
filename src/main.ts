01b13a5c8f51e2c7ced4378042d0137ccbc4b24c
Hayeon Baek <bhy0512@gmail.com>
2026-01-24 12:50:15 +0900

    feat: Swagger API 문서 설정 및 JSON 경로 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API description')
+    .setVersion('1.0.0')
+    .addTag('')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: 'docs/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

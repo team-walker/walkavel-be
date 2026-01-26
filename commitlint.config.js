a5e921754031f66db579183c8299313d94d7973c
Hayeon Baek <bhy0512@gmail.com>
2026-01-26 14:15:58 +0900

    chore: commitlint 설정을 표준 conventional 설정으로 복구

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [0], // Disable subject-case to support Korean and acronyms like README/PR
-  },
-};
+module.exports = { extends: ['@commitlint/config-conventional'] };
